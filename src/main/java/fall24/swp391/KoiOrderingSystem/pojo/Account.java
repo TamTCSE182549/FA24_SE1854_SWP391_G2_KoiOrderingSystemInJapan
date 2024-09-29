e8314f3956d0888eb2162576f6bf159ed9b359f3
MinhAlfred <minhvqse181556@fpt.edu.vn>
2024-09-29 10:55:07 +0700

    pojo cua minh vo

--- src/main/java/fall24/swp391/KoiOrderingSystem/pojo/Account.java
+++ src/main/java/fall24/swp391/KoiOrderingSystem/pojo/Account.java
@@ -1,7 +1,33 @@
 package fall24.swp391.KoiOrderingSystem.pojo;
 
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class Account {
-    private String userName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Email(message = "Email not valid")
+    @Column(unique = true)
+    private String email;
+
+    @Pattern(regexp = "(84|0[3|5|7|8|9])+(\\d{8})",message = "Phone invalid")
+    @Column(name = "phone_number",unique = true)
+    private String phone;
+
+    @NotBlank(message = "password can not blank")
+    @Size(min = 6,message = "Password must be at least 6 characters!")
+    private String password;
+
+    @Column(name = "is_active")
+    private boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
