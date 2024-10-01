c6de8d6958cd3d2536825b611726e008f5fd75ae
TranChiTam <tranchitam65a@gmail.com>
2024-10-02 00:21:13 +0700

    update repo

--- src/main/java/fall24/swp391/KoiOrderingSystem/repo/ITourDetailRepository.java
+++ src/main/java/fall24/swp391/KoiOrderingSystem/repo/ITourDetailRepository.java
@@ -1,8 +1,9 @@
 package fall24.swp391.KoiOrderingSystem.repo;
 
+import fall24.swp391.KoiOrderingSystem.pojo.TourDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ITourDetailRepository extends JpaRepository<ITourRepository, Long> {
+public interface ITourDetailRepository extends JpaRepository<TourDetail, Long> {
 }
