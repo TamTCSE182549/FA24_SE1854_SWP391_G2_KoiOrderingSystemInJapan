a4c9fd5f772498fa5313a15e177645799c6088f3
TranChiTam <tranchitam65a@gmail.com>
2024-10-04 23:24:11 +0700

    update pojo and service

--- src/main/java/fall24/swp391/KoiOrderingSystem/service/TourService.java
+++ src/main/java/fall24/swp391/KoiOrderingSystem/service/TourService.java
@@ -1,4 +1,33 @@
 package fall24.swp391.KoiOrderingSystem.service;
 
+import fall24.swp391.KoiOrderingSystem.pojo.Tours;
+import fall24.swp391.KoiOrderingSystem.repo.ITourRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class TourService implements ITourService{
+
+    private ITourRepository iTourRepository;
+
+    @Override
+    public Tours createTour(Tours tours){
+        return iTourRepository.save(tours);
+    }
+
+    @Override
+    public boolean updateTour(Long id, Tours tours) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteTour(Long id){
+        Optional<Tours> tours = iTourRepository.findById(id);
+        if(tours.isPresent()){
+            iTourRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
