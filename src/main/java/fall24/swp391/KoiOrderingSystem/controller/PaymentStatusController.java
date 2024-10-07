596e569f4d02c1e5fa587cd5727ab186abaaf0fd
kphan <khoipmse182554@fpt.edu.vn>
2024-10-07 14:12:30 +0700

    add Service

--- src/main/java/fall24/swp391/KoiOrderingSystem/controller/PaymentStatusController.java
+++ src/main/java/fall24/swp391/KoiOrderingSystem/controller/PaymentStatusController.java
@@ -1,4 +1,17 @@
 package fall24.swp391.KoiOrderingSystem.controller;
 
+
+import fall24.swp391.KoiOrderingSystem.service.IBookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping
 public class PaymentStatusController {
+
+    @Autowired
+    private IBookingService iBookingService;
 }
--- src/main/java/fall24/swp391/KoiOrderingSystem/service/IKoisService.java
+++ src/main/java/fall24/swp391/KoiOrderingSystem/service/IKoisService.java
@@ -1,4 +1,15 @@
 package fall24.swp391.KoiOrderingSystem.service;
 
+import fall24.swp391.KoiOrderingSystem.pojo.Kois;
+
+import java.util.List;
+
 public interface IKoisService {
+    List<Kois> findAll();
+
+    Kois findById(Long Id);
+
+    Kois save(Kois kois);
+
+    void deletebyId(Long Id);
 }
