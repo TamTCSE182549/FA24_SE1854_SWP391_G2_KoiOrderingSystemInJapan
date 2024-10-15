3b050f2afc87fc55ba7ae162b8cb8de9ae1d9e29
kphan <khoipmse182554@fpt.edu.vn>
2024-10-15 20:43:44 +0700

    Category

--- src/main/java/fall24/swp391/KoiOrderingSystem/service/ICategoriesService.java
+++ src/main/java/fall24/swp391/KoiOrderingSystem/service/ICategoriesService.java
@@ -1,15 +1,24 @@
 package fall24.swp391.KoiOrderingSystem.service;
 
+import fall24.swp391.KoiOrderingSystem.model.request.CategoryRequest;
+import fall24.swp391.KoiOrderingSystem.model.response.CategoryResponse;
 import fall24.swp391.KoiOrderingSystem.pojo.Categories;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ICategoriesService {
     List<Categories> findAll();
 
-    Categories findById(Long Id);
+    Optional<Categories> findById(Long id);
 
-    Categories save(Categories categories);
+    CategoryResponse createCategory(CategoryRequest categoryRequest);
 
-    void deletebyId(Long Id);
+    CategoryResponse updateCategory(CategoryRequest categoryRequest, Long id);
+
+//    CategoryResponse addKoiToCategory(Long categoryId, Long koiId);
+//
+//    CategoryResponse getCategoryWithKoiById(Long categoryId);
+//
+//    CategoryResponse convertToCategoryWithKoi(Categories categories);
 }
