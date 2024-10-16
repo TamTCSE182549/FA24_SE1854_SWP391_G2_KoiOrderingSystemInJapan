2c5ff55870dc857b52db418f5185e2eb7f61a499
kphan <khoipmse182554@fpt.edu.vn>
2024-10-16 07:10:50 +0700

    KoiFarmImage

---
+++ src/main/java/fall24/swp391/KoiOrderingSystem/model/response/KoiFarmImageResponse.java
@@ -0,0 +1,13 @@
+package fall24.swp391.KoiOrderingSystem.model.response;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class KoiFarmImageResponse {
+    private Long id;
+    private String imageUrl;
+}
