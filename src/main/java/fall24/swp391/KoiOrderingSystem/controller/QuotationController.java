5d54f7aabffaed254d941e414862e69f202b87a5
kphan <khoipmse182554@fpt.edu.vn>
2024-10-21 21:28:50 +0700

    upQuotation

--- src/main/java/fall24/swp391/KoiOrderingSystem/controller/QuotationController.java
+++ src/main/java/fall24/swp391/KoiOrderingSystem/controller/QuotationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/quotations")
+@RequestMapping("/quotations")
 @CrossOrigin(origins = "*")
 @SecurityRequirement(name = "api")
 public class QuotationController {
